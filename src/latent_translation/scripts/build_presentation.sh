162fd66da77cc598508af9146cd219036fde8730
Valentino Maiorca <valentino@maiorca.xyz>
2023-11-12 20:32:10 +0100

    Add build script

---
+++ src/latent_translation/scripts/build_presentation.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+manim --save_sections src/latent_translation/manim/slides/title.py Title
+manim --save_sections src/latent_translation/manim/slides/teaser.py Teaser
+manim --save_sections src/latent_translation/manim/slides/method.py Method
+manim --save_sections src/latent_translation/manim/slides/algorithm.py Algorithm
+manim --save_sections src/latent_translation/manim/slides/stitching.py Stitching
+manim --save_sections src/latent_translation/manim/slides/crossmodal.py Crossmodal
+manim --save_sections src/latent_translation/manim/slides/conclusions.py Conclusions
+manim --save_sections src/latent_translation/manim/slides/thanks.py Thanks
