1fb6fbb325fa4e0541db16bf686a21a91f9b4263
Valentino Maiorca <valentino@maiorca.xyz>
2023-11-14 10:11:31 +0100

    Update build and export scripts

--- src/latent_translation/scripts/build_presentation.sh
+++ src/latent_translation/scripts/build_presentation.sh
@@ -2,7 +2,7 @@
 
 manim --save_sections src/latent_translation/manim/slides/title.py Title
 manim --save_sections src/latent_translation/manim/slides/teaser.py Teaser
-manim --save_sections src/latent_translation/manim/slides/method.py Method
+manim --save_sections src/latent_translation/manim/slides/translation.py Translation
 manim --save_sections src/latent_translation/manim/slides/algorithm.py Algorithm
 manim --save_sections src/latent_translation/manim/slides/stitching.py Stitching
 manim --save_sections src/latent_translation/manim/slides/crossmodal.py Crossmodal
---
+++ src/latent_translation/scripts/export_presentation.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+manedit --project_name "LatentTranslation" \
+  --quick_present_export media/videos/title/1080p60/sections/Title.json \
+  --quick_present_export media/videos/teaser/1080p60/sections/Teaser.json \
+  --quick_present_export media/videos/translation/1080p60/sections/Translation.json \
+  --quick_present_export media/videos/algorithm/1080p60/sections/Algorithm.json \
+  --quick_present_export media/videos/stitching/1080p60/sections/Stitching.json \
+  --quick_present_export media/videos/crossmodal/1080p60/sections/Crossmodal.json \
+  --quick_present_export media/videos/conclusions/1080p60/sections/Conclusions.json \
+  --quick_present_export media/videos/thanks/1080p60/sections/Thanks.json
